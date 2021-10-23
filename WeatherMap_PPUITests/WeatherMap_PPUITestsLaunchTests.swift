ffa5c63be9558648bd87abc5fa26edbaa3c1df41
MorozovDeveloper <76910221+MorozovDeveloper@users.noreply.github.com>
2021-10-23 11:16:17 +0300

    Initial Commit

---
+++ WeatherMap_PPUITests/WeatherMap_PPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherMap_PPUITestsLaunchTests.swift
+//  WeatherMap_PPUITests
+//
+//  Created by Oleg on 23.10.2021.
+//
+
+import XCTest
+
+class WeatherMap_PPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
