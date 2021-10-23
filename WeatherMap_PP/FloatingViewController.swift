7b71ebd99c3150542f95003df3e153072c8d039d
MorozovDeveloper <76910221+MorozovDeveloper@users.noreply.github.com>
2021-10-23 13:57:46 +0300

    Floating View Controller

---
+++ WeatherMap_PP/FloatingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FloatingViewController.swift
+//  WeatherMap_PP
+//
+//  Created by Oleg on 23.10.2021.
+//
+
+import UIKit
+
+class FloatingViewController: UIViewController {
+    
+    @IBOutlet weak var handleArea: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
--- WeatherMap_PP/ViewController.swift
+++ WeatherMap_PP/ViewController.swift
@@ -7,17 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
+    // положение FloatingVC
+    enum CardState{
+        case expanded // расширеный
+        case collapsed // сжатый
+    }
     
+    var floatingViewController : FloatingViewController!
+    
+    let cardHeight: CGFloat = 400
+    let cardHandleAreaHeight: CGFloat = 65
+    
+    var cardVisible = false
+    var nextState: CardState{
+        return cardVisible ? .collapsed : .expanded
+    }
+    
+    var runningAnimations = [UIViewPropertyAnimator]()
+    var animationProgressWhenInterrupted: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     //www
-        
-        /// sgdfgdfhdfh
+        setupCard()
     }
 
+    func setupCard() {
+        floatingViewController = FloatingViewController(nibName: "FloatingViewController", bundle: nil)
+        self.addChild(floatingViewController)
+        self.view.addSubview(floatingViewController.view)
+        
+        floatingViewController.view.frame = CGRect(x: 0,
+                                                   y: self.view.frame.height - cardHandleAreaHeight,
+                                                   width: self.view.bounds.width,
+                                                   height: cardHeight)
+        floatingViewController.view.clipsToBounds = true
+        
+        // Gesture
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                          action: #selector(MapViewController.handleCardTap(recognizer:)))
+        floatingViewController.handleArea.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc
+    func handleCardTap(recognizer: UITapGestureRecognizer){
+        switch recognizer.state{
+        case .ended:
+            animateTransitionIfNeedet(state: nextState, duration: 0.9)
+        default:
+            break
+        }
+    }
+    
+    func animateTransitionIfNeedet(state: CardState, duration: TimeInterval) {
+        if  runningAnimations.isEmpty {
+            let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                switch state {
+                case .expanded:
+                    self.floatingViewController.view.frame.origin.y = self.view.frame.height - self.cardHeight
+                case .collapsed:
+                    self.floatingViewController.view.frame.origin.y = self.view.frame.height - self.cardHandleAreaHeight
+                }
+            }
+            
+            frameAnimator.addCompletion { _ in
+                self.cardVisible = !self.cardVisible
+                self.runningAnimations.removeAll()
+            }
+            
+            frameAnimator.startAnimation()
+            runningAnimations.append(frameAnimator)
+        }
+    }
+    
 
 }
 
