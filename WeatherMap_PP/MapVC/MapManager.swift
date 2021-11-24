01dea223d9df0ce2f94ec997a7ba24241e60030b
MorozovDeveloper <76910221+MorozovDeveloper@users.noreply.github.com>
2021-11-24 21:16:09 +0300

    use Current Weather

--- WeatherMap_PP/MapVC/MapManager.swift
+++ WeatherMap_PP/MapVC/MapManager.swift
@@ -5,49 +5,63 @@
 //  Created by Oleg on 24.10.2021.
 //
 
+import UIKit
 import Foundation
 import MapKit
 import CoreLocation
 
 
+// Создаем маркер и баннер
+extension MapViewController: MKMapViewDelegate {
+    
+    // Метка
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 
-//class MapManager, CLLocationManagerDelegate {
-//
-//    @IBOutlet weak var mapView: MKMapView!
-//    
-//let manager = CLLocationManager()
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
-//    
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        manager.desiredAccuracy = kCLLocationAccuracyBest // battery
-//        manager.delegate = self
-//        manager.requestWhenInUseAuthorization()
-//        manager.startUpdatingLocation()
-//        
-//    }
-//    
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        if let location = locations.first {
-//            manager.stopUpdatingLocation()
-//            
-//            render(location)
-//        }
-//    }
-//    
-//    func render(_ location: CLLocation) {
-//        
-//        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
-//                                                longitude: location.coordinate.longitude)
-//        let span = MKCoordinateSpan(latitudeDelta: 0.1,
-//                                    longitudeDelta: 0.1)
-//        let region = MKCoordinateRegion(center: coordinate,
-//                                        span: span)
-//        
-//        mapView.setRegion(region, animated: true)
-//    }
-//    
-//}
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+
+        func render(_ location: CLLocation) {
+            let coordinate = CLLocationCoordinate2D(latitude: self.latCor,
+                                                        longitude: self.lonCor)
+            let span = MKCoordinateSpan(latitudeDelta: 1.0,
+                                        longitudeDelta: 1.0) // маштаб
+
+            let region = MKCoordinateRegion(center: coordinate,
+                                            span: span)
+
+            self.mapView.setRegion(region, animated: true)
+
+            let pin = MKPointAnnotation()
+            pin.coordinate = coordinate
+            self.mapView.addAnnotation(pin)
+                pin.title = self.pinText
+                pin.subtitle = "\(self.subText)"
+        }
+    }
+    
+    // Баннер
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else {return nil}
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotaionIdentifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: self.annotaionIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 25, height: 25))
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        //imageView.image = UIImage(systemName: "cloud")
+        imageView.image = image
+        annotationView?.rightCalloutAccessoryView = imageView
+    
+        return annotationView
+    }
+
+        
+}
+
